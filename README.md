f5ce10293d2726b6c982ea0b592b2f1cef8d81b4
VincentG <vincentpyg@users.noreply.github.com>
2017-05-01 10:21:38 +1000

    Added readme, Fixed bugs in config validation

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# java-article-crawler
+## java-article-crawler
+
+Web crawler used to crawl and extract information from articles in bbc.com and theguardian.com
+
+### Commandline Arguments
+
+|Parameter|Value|Required|Default|
+|---|---|---|---|
+|`-c`|configuration properties file|Yes|N/A|
+|`-d`|Crawl depth where `0` = source page only, `1` = one level of links from the source page, etc.|No|0|
+|`-t`|Number of crawl threads|No|1|
+
+### Configuration properties file
+|Configuration Parameter|Value|
+|---|---|
+|`sourceURL`|Source URL where the crawl will start|
+|`crawlPattern`|Regex pattern to identify pages that should be crawled|
+|`authorPattern`|Regex pattern used to extract the author of the article|
+|`headlinePattern`|Regex pattern used to extract the headline of the article|
+|`contentPattern`|class name where the article's content is located|
+|`mongoURI`|mongodb URI (complete server address)|
+|`mongoDB`|mongodb database name|
+|`mongoCollection`|mondodb collection where the articles will be stored|
+
+### Running the application
+```$Bash
+$ java -jar ${APPLICATION_JAR} -d ${CRAWL_DEPTH} -t ${CRAWL_THREADS} -c ${CONFIG_PROPERTIES_FILE}
+
+```
--- src/main/java/com/vincentg/util/PropertiesValidator.java
+++ src/main/java/com/vincentg/util/PropertiesValidator.java
@@ -18,10 +18,13 @@ public class PropertiesValidator implements IValueValidator<Properties> {
     public void validate(String name, Properties value) throws ParameterException {
         try {
             Validate.notEmpty(value.getProperty(CONFIG_SOURCE_URL),CONFIG_SOURCE_URL+" is missing");
-            Validate.notEmpty(value.getProperty(CONFIG_CRAWL_PATTERN),CONFIG_SOURCE_URL+" is missing");
-            Validate.notEmpty(value.getProperty(CONFIG_AUTHOR_PATTERN),CONFIG_SOURCE_URL+" is missing");
-            Validate.notEmpty(value.getProperty(CONFIG_HEADLINE_PATTERN),CONFIG_SOURCE_URL+" is missing");
-            Validate.notEmpty(value.getProperty(CONFIG_CONTENT_PATTERN),CONFIG_SOURCE_URL+" is missing");
+            Validate.notEmpty(value.getProperty(CONFIG_CRAWL_PATTERN),CONFIG_CRAWL_PATTERN+" is missing");
+            Validate.notEmpty(value.getProperty(CONFIG_AUTHOR_PATTERN),CONFIG_AUTHOR_PATTERN+" is missing");
+            Validate.notEmpty(value.getProperty(CONFIG_HEADLINE_PATTERN),CONFIG_HEADLINE_PATTERN+" is missing");
+            Validate.notEmpty(value.getProperty(CONFIG_CONTENT_PATTERN),CONFIG_CONTENT_PATTERN+" is missing");
+            Validate.notEmpty(value.getProperty(CONFIG_MONGO_URI),CONFIG_MONGO_URI+" is missing");
+            Validate.notEmpty(value.getProperty(CONFIG_MONGO_DB),CONFIG_MONGO_DB+" is missing");
+            Validate.notEmpty(value.getProperty(CONFIG_MONGO_COLLECTION),CONFIG_MONGO_COLLECTION+" is missing");
         }
         catch (IllegalArgumentException e) {
             throw new ParameterException("parameter "+name+" has invalid properties: "+e.getMessage());
